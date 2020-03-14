c51346813b303106513159cf7eb8508733e4888f
bammss <bambangsugiarto200316@gmail.com>
2020-03-14 21:40:12 +0700

    add histogram

--- Main.py
+++ Main.py
@@ -1,4 +1,13 @@
 import pandas as pd
+from matplotlib import pyplot
 path = "datasets\COVID19_line_list_data.csv"
 data = pd.read_csv(path)
+print("INFO DATASET CORONA")
 print(data)
+print(data.describe())
+print(data.info())
+print("RATA-RATA SEMUA KOLOM")
+print(data.mean())
+print("TAMPIL HISTOGRAM")
+data.hist()
+pyplot.show()
